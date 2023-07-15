4f04c5197e7788bdc8e777d0c69c1d6aa99de079
LucasFoulkes <lucasfoulkes@gmail.com>
2023-07-15 02:21:49 -0500

    the process route is still not working properly

--- dog-clothes/src/components/ImageWithMask.jsx
+++ dog-clothes/src/components/ImageWithMask.jsx
@@ -1,8 +1,12 @@
-/* eslint-disable react/prop-types */
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const ImageWithMask = ({ originalImage, maskImage }) => {
+const ImageWithMask = ({ originalImage, maskImage, setEditedMask }) => {
   const canvasRef = useRef(null);
+  const mousePos = useRef({ x: 0, y: 0 });
+  const mousePressed = useRef(false);
+  const circleRadius = useRef(50);
+  const [radius, setRadius] = useState(50);
+  let animationFrameId;
 
   const editPixels = (data) => {
     for (let i = 0; i < data.length; i += 4) {
@@ -19,6 +23,39 @@ const ImageWithMask = ({ originalImage, maskImage }) => {
     }
   };
 
+  const resetCanvas = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    const img = new Image();
+    img.src = URL.createObjectURL(originalImage);
+
+    const mask = new Image();
+    mask.src = maskImage;
+
+    Promise.all([
+      new Promise((resolve) => {
+        img.onload = resolve;
+      }),
+      new Promise((resolve) => {
+        mask.onload = resolve;
+      }),
+    ]).then(() => {
+      context.drawImage(mask, 0, 0, canvas.width, canvas.height);
+
+      let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      editPixels(imageData.data);
+      context.putImageData(imageData, 0, 0);
+    });
+  };
+
+  const handleSave = () => {
+    const canvas = canvasRef.current;
+    const dataUrl = canvas.toDataURL();
+    setEditedMask(dataUrl);
+  };
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
@@ -29,26 +66,66 @@ const ImageWithMask = ({ originalImage, maskImage }) => {
     const mask = new Image();
     mask.src = maskImage;
 
-    const loadImages = [
+    const handleMouseMove = (event) => {
+      const rect = canvas.getBoundingClientRect();
+      mousePos.current = {
+        x: ((event.clientX - rect.left) * canvas.width) / rect.width,
+        y: ((event.clientY - rect.top) * canvas.height) / rect.height,
+      };
+    };
+
+    const handleMouseDown = () => {
+      mousePressed.current = true;
+    };
+
+    const handleMouseUp = () => {
+      mousePressed.current = false;
+    };
+
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    Promise.all([
       new Promise((resolve) => {
         img.onload = resolve;
       }),
       new Promise((resolve) => {
         mask.onload = resolve;
       }),
-    ];
-
-    Promise.all(loadImages).then(() => {
+    ]).then(() => {
       canvas.width = img.width;
       canvas.height = img.height;
 
       context.drawImage(mask, 0, 0, canvas.width, canvas.height);
 
-      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
       editPixels(imageData.data);
-
       context.putImageData(imageData, 0, 0);
+
+      const render = () => {
+        if (mousePressed.current) {
+          context.save();
+          context.translate(mousePos.current.x, mousePos.current.y);
+          context.fillStyle = "#000000";
+          context.beginPath();
+          context.arc(0, 0, circleRadius.current, 0, 2 * Math.PI, false);
+          context.fill();
+          context.restore();
+        }
+
+        animationFrameId = requestAnimationFrame(render);
+      };
+
+      render();
     });
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
   }, [originalImage, maskImage]);
 
   return (
@@ -60,8 +137,32 @@ const ImageWithMask = ({ originalImage, maskImage }) => {
       />
       <canvas
         ref={canvasRef}
-        className="absolute top-0 left-0 h-full opacity-80"
+        className="absolute top-0 left-0 h-full opacity-30"
       />
+      <input
+        type="range"
+        min="10"
+        max="200"
+        value={radius}
+        onChange={(e) => {
+          setRadius(e.target.value);
+          circleRadius.current = e.target.value;
+        }}
+        className="absolute top-0 right-0"
+      />
+      <button
+        onClick={resetCanvas}
+        className="absolute top-0 right-0 m-4 bg-zinc-900"
+      >
+        Reset
+      </button>
+      <br />
+      <button
+        onClick={handleSave}
+        className="absolute top-20 right-0 m-4 bg-zinc-900"
+      >
+        Save
+      </button>
     </div>
   );
 };
---
+++ dog-clothes/src/components/MaskedImage.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const MaskedImage = ({ editedMask, originalImage }) => {
+  const [processedImage, setProcessedImage] = useState(null);
+  const [originalImageUrl, setOriginalImageUrl] = useState(null);
+  const [editedImageUrl, setEditedImageUrl] = useState(null);
+
+  const processImage = async () => {
+    const responseOriginal = await fetch(originalImage);
+    const blobOriginal = await responseOriginal.blob();
+    const fileOriginal = new File([blobOriginal], "original.png", {
+      type: "image/png",
+    });
+
+    const responseEdited = await fetch(editedMask);
+    const blobEdited = await responseEdited.blob();
+
+    // Create an image object
+    const img = new Image();
+    img.src = URL.createObjectURL(blobEdited);
+    await new Promise((resolve) => (img.onload = resolve));
+
+    // Create a canvas and draw the image onto it
+    const canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    // Convert the image to grayscale
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+      data[i] = avg; // red
+      data[i + 1] = avg; // green
+      data[i + 2] = avg; // blue
+    }
+    ctx.putImageData(imageData, 0, 0);
+
+    // Convert the canvas to a blob
+    const editedBlob = await new Promise((resolve) =>
+      canvas.toBlob(resolve, "image/png")
+    );
+    const fileEdited = new File([editedBlob], "edited.png", {
+      type: "image/png",
+    });
+
+    const formData = new FormData();
+    formData.append("file", fileOriginal);
+    formData.append("prediction", fileEdited);
+
+    const serverResponse = await axios.post(
+      "http://localhost:5000/process",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        responseType: "blob",
+      }
+    );
+
+    const url = URL.createObjectURL(serverResponse.data);
+    setProcessedImage(url);
+    setOriginalImageUrl(URL.createObjectURL(fileOriginal));
+    setEditedImageUrl(URL.createObjectURL(fileEdited));
+  };
+
+  useEffect(() => {
+    if (editedMask) {
+      processImage();
+    }
+  }, [editedMask]);
+
+  return (
+    <div>
+      {originalImageUrl && (
+        <img
+          src={originalImageUrl}
+          alt="Original"
+          style={{ width: "100px", height: "100px", objectFit: "contain" }}
+        />
+      )}
+      {editedImageUrl && (
+        <img
+          src={editedImageUrl}
+          alt="Edited"
+          style={{ width: "100px", height: "100px", objectFit: "contain" }}
+        />
+      )}
+      {processedImage && (
+        <img
+          src={processedImage}
+          alt="Processed"
+          style={{ width: "100px", height: "100px", objectFit: "contain" }}
+        />
+      )}
+      <button onClick={processImage} className="bg-zinc-900">
+        Process
+      </button>
+    </div>
+  );
+};
+
+export default MaskedImage;
